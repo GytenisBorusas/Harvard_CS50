51f1ecad4a7c49f1b6c97c90726c66917d0438fa
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-08-20 19:55:47 -0700

    modified:   google-python-exercises/google-python-exercises/basic/list1.py

--- google-python-exercises/google-python-exercises/basic/list1.py
+++ google-python-exercises/google-python-exercises/basic/list1.py
@@ -61,7 +61,8 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-  
+  sorted(tuples)
+  return tuples
   # +++your code here+++
   return
 
