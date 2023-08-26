1789df370cc3c8b9f72159aefc34ff5fcb8c78fc
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-08-26 11:36:42 -0700

    modified:   Harvard CS50/calculator.py 	modified:   Harvard CS50/hello.py

--- Harvard CS50/hello.py
+++ Harvard CS50/hello.py
@@ -1,10 +1,28 @@
-# Ask user for their name
-name = input("What is your name? ") 
-
 # Remote whitespace from str
-name = name.strip()
+# name = name.strip()
+
+# Capitalize first letter of each word
+# name = name.title()
+
+# Split user's name into first name and last name
+# first, last = name.split()
 
 # Print out "Hellow + user name from the previous line"
-print(f"Hello, {name}")
+# print(f"Hello, {first}")
+
+
+def main():
+    name = input("What is your name? ") 
+    hello(name)
+
+
+def hello(to="World"):
+    print("Hello,", to)
+
+
+main()
+
+
+
 
 
