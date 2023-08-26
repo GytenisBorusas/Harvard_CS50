9bae2b6b154348ce539380a0956ef6b3afe0aca6
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-08-26 12:01:50 -0700

    modified:   Harvard CS50/compare.py

--- Harvard CS50/compare.py
+++ Harvard CS50/compare.py
@@ -5,5 +5,7 @@ y = int(input("What is y? "))
 
 if x < y:
     print("x is less than y")
-else:
-    print("x is greater than or equal to y")
+elif x > y:
+    print("x is greater than y")
+else x == y:
+    print("x is equal to y")
