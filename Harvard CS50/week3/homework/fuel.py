6b4631253a7e099fc2e519df61af0d79fa45c11b
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-01 15:23:35 -0700

    first fuel.py commit

--- Harvard CS50/week3/homework/fuel.py
+++ Harvard CS50/week3/homework/fuel.py
@@ -1,3 +1,19 @@
 
+
 def main():
-    
+    x = get_fuel_amount("Fraction? ")
+    print(f"x is {x}")
+
+def get_fuel_amount(prompt):
+    while True:
+        try:
+            x = int(input(prompt))
+        except ValueError:
+            print("This isnt a fraction")
+        except ZeroDivisionError:
+            print("Cant divide by 0")
+        else:
+            return x
+        
+
+main()
