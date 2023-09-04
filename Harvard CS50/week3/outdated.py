08db2f1e105ea71405fa4e84b9224ed91a14c50c
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-04 14:08:41 -0600

    initial outdated.py commit

---
+++ Harvard CS50/week3/outdated.py
@@ -0,0 +1,20 @@
+
+def main():
+
+    month_names = {
+        "january": ["january", "1"],
+        "february": ["february", "2"],
+        "march": ["march", "3"],
+        "april": ["april", "4"],
+        "may": ["may", "5"],
+        "june": ["june", "6"],
+        "july": ["july", "7"],
+        "august": ["august", "8"],
+        "september": ["september", "9"],
+        "october": ["october", "10"],
+        "november": ["november", "11"],
+        "december": ["december", "12"]
+}
+
+    user_input = input("Date: ")
+    user_input.split
