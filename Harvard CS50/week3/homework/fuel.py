4164985117259b3e6d21707c71a3f38e58749795
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-03 17:26:59 -0700

    fuel.py halfway done

--- Harvard CS50/week3/homework/fuel.py
+++ Harvard CS50/week3/homework/fuel.py
@@ -1,19 +1,32 @@
 
 
 def main():
-    x = get_fuel_amount("Fraction? ")
-    print(f"x is {x}")
+    fuel_fraction = get_fuel_amount("Fraction? ")
 
-def get_fuel_amount(prompt):
-    while True:
-        try:
-            x = int(input(prompt))
-        except ValueError:
-            print("This isnt a fraction")
-        except ZeroDivisionError:
-            print("Cant divide by 0")
-        else:
-            return x
+    # divides input 'fuel_fraction' into 2 different variables - 'x' and 'y'
+    x, y = fraction_input.split('/')
+
+    # Converts 'x' and 'y' to integers
+    x = int(x)
+    y = int(y)
+
+    fuel_amount = x / y * 100
+    print(f"{fuel_amount}")
+
+
+
+
+
+# def get_fuel_amount(prompt):
+#     while True:
+#         try:
+#             x = int(input(prompt))
+#         except ValueError:
+#             print("This isnt a fraction")
+#         except ZeroDivisionError:
+#             print("Cant divide by 0")
+#         else:
+#             return x
         
 
 main()
