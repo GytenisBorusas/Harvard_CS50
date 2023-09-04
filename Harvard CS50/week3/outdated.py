d70247397ab2bd6f0e84810faa1b7f626539d335
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-04 14:44:23 -0600

    modified:   Harvard CS50/week3/outdated.py

--- Harvard CS50/week3/outdated.py
+++ Harvard CS50/week3/outdated.py
@@ -18,31 +18,46 @@ month_names = {
 }
 
 
-def main():
-
-    user_date_input = identify_format("Date: ")
-    print(f"{user_date_input}")
-
-
-
+def get_month_number(month_name):
+    for key, values in month_names.items():
+        if month_name.lower() in values:
+            return values[1]
+    return None  # Return None if month not found
 
 
 def identify_format(prompt):
-    # get input from the user
     date_str = input(prompt)
 
     # Check for the "9/8/1636" format
     if re.match(r"\d{1,2}/\d{1,2}/\d{4}", date_str):
-        return "Short format"
+        month, day, year = date_str.split('/')
+        return day, month, year
 
     # Check for the "September 8, 1636" format
     try:
         datetime.strptime(date_str, '%B %d, %Y')
-        return "Long format"
+        date_part, year = date_str.split(', ')
+        month_name, day = date_part.split(' ')
+        month = get_month_number(month_name)
+        if not month:  # If month not found, raise a ValueError
+            raise ValueError
+        return day, month, year
+
     except ValueError:
         pass
 
     return "Unknown format"
 
+
+
+
+def main():
+
+    day, month, year = identify_format("Date: ")
+
+
+    print("Month:", month)
+    print("Day:", day)
+    print("Year:", year)
 
 main()
