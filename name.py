22f50b9a963cb3794939d1104f07828b3640e8dd
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-05 12:33:44 -0600

    cowsay library working

--- name.py
+++ name.py
@@ -20,12 +20,23 @@
 
 #---------------------------------------------------------------------
 
+# import sys
+
+# if len(sys.argv) < 2:
+#     sys.exit("Too few arguments")
+# elif len(sys.argv)> 2:
+#     sys.exit("Too many arguments")
+
+# print("hello, my name is", sys.argv[1])
+
+
+#---------------------------------------------------------------------
+
 import sys
 
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
-elif len(sys.argv)> 2:
-    sys.exit("Too many arguments")
 
 
-print("hello, my name is", sys.argv[1])
+for name in sys.argv[1:]:
+    print("hello, my name is", name)
