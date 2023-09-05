2d08c1be609417f6d63de40f55cab3f3010bafca
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-05 11:42:09 -0600

    Sept 5, first commit

--- name.py
+++ name.py
@@ -1,8 +1,19 @@
 
+# import sys
+
+# try:
+#     print("hello, my name is", sys.argv[1])
+# except IndexError:
+#     print("Too few arguments")
+
+
+#---------------------------------------------------------------------
+
 import sys
 
-print("hello, my name is", sys.argv[1])
-
-
-
-
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv)> 2:
+    print("Too many arguments")
+else:
+    print("hello, my name is", sys.argv[1])
