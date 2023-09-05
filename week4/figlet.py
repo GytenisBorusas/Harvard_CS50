2d6bb40e08b4e7b948a24acb1217c601c7664de2
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-05 16:03:46 -0600

    figlet.py done and tested

--- week4/figlet.py
+++ week4/figlet.py
@@ -2,26 +2,32 @@ import sys
 from pyfiglet import Figlet
 
 def main():
+    # Only one additional argument, check if it's a font flag or an error
+    if len(sys.argv) == 2:
+        sys.exit("Error message #1")  # There's no font name provided after the flag
+    # Invalid flag
+    elif len(sys.argv) == 3 and sys.argv[1] not in ["-f", "--font"]:
+        sys.exit("Error message #1")
+    # Invalid font or more than 2 additional arguments: exit with an error
+    elif len(sys.argv) == 3:
+        try:
+            # Attempt to create a Figlet object to see if the font is valid
+            Figlet(font=sys.argv[2])
+        except Exception:
+            sys.exit("Error message #1")
+    elif len(sys.argv) > 3:
+        sys.exit("Error message #1")
+    
+    # At this point, all errors related to command-line arguments have been checked
     user_input = input("Input: ")
 
     # No extra arguments, use default 'slant' font
     if len(sys.argv) == 1:
         f = Figlet(font='slant')
-        print(f.renderText(user_input))
-    # Only one additional argument, check if it's a font flag or an error
-    elif len(sys.argv) == 2:
-        sys.exit("Error message #1")  # There's no font name provided after the flag
-    # Two additional arguments: should be font flag + font name
-    elif len(sys.argv) == 3:
-        if sys.argv[1] in ["-f", "--font"]:
-            # Use the provided font
-            f = Figlet(font=sys.argv[2])
-            print(f.renderText(user_input))
-        else:
-            sys.exit("Error message #1")  # Invalid flag
-    # More than 2 additional arguments is an error
+    # Two additional arguments: use the provided font
     else:
-        sys.exit("Error message #2")
+        f = Figlet(font=sys.argv[2])
+    print(f.renderText(user_input))
 
 if __name__ == "__main__":
     main()
