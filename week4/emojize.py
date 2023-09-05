48420540b41661d9500a43b92f0d0ed3cecac0a3
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-05 15:18:52 -0600

    dine with emojize.y

--- week4/emojize.py
+++ week4/emojize.py
@@ -4,8 +4,11 @@ from emoji import emojize
 def main():
     user_input = input("Input: ")
 
-    emojized_input = emojize(user_input, language='en')
+    emojized_input = emojize(user_input, language='en', use_aliases=True)
     print(emojized_input)
 
 if __name__ == "__main__":
     main()
+
+
+ 
