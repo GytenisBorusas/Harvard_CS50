925d2583e08349cf50357e87ec3d35df900c7cfb
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-06 18:18:39 -0600

    game.py troubleshooting

--- week4/game.py
+++ week4/game.py
@@ -1,1 +1,22 @@
 
+import random
+
+def main():
+    user_level_dificulty = int(input("Level: "))
+
+    random_number = random.randrange(1, user_level_dificulty + 1)
+    print(random_number)
+
+    user_guess = int(input("Guess: "))
+    while user_guess != random_number:
+        if user_level_dificulty > user_guess > random_number:
+            print("Too large!")
+        elif 0 < user_guess < random_number:
+            print("Too small!")
+        
+        user_guess = int(input("Guess: "))
+    
+    print("Just right!")
+
+main()
+
