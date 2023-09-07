d520938dd5fd7f331695abe3c2f40e19229757fc
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-06 18:43:31 -0600

    added not an integer error catches

--- week4/game.py
+++ week4/game.py
@@ -2,21 +2,48 @@
 import random
 
 def main():
-    user_level_dificulty = int(input("Level: "))
-
+    while True:
+        try:
+            user_level_dificulty = int(input("Level: "))
+            break
+        except ValueError:
+            print("Not a valid number")
+        
     random_number = random.randrange(1, user_level_dificulty + 1)
     print(random_number)
 
-    user_guess = int(input("Guess: "))
+    congratulation_message = guessing_game(random_number, user_level_dificulty)
+    print(congratulation_message)
+
+
+
+
+def guessing_game(random_number, user_level_dificulty):
+    while True:
+        try:
+            user_guess = int(input("Guess: "))
+            break
+        except ValueError:
+            print("Not a valid number")
+
     while user_guess != random_number:
         if user_level_dificulty > user_guess > random_number:
             print("Too large!")
         elif 0 < user_guess < random_number:
             print("Too small!")
-        
-        user_guess = int(input("Guess: "))
-    
-    print("Just right!")
 
-main()
+        while True:
+            try:
+                user_guess = int(input("Guess: "))
+                break
+            except ValueError:
+                print("Not a valid number")
+
+    return "Just right"
+        
+
+if __name__ == "__main__":
+    main()
+
+
 
