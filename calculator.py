e4530430b384e6d1f5dca29f6cab60e9bf04d0e3
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-08 16:53:25 -0600

    week classroom work

--- calculator.py
+++ calculator.py
@@ -6,11 +6,11 @@
 # print(f"{z:,}")
 
 def main():
-    x = float(input("What is x? "))
+    x = int(input("What is x? "))
     print("x squared is", square(x))
 
 def square(n):
-    return n * n
+    return n + n
 
 if __name__ == "__main__":
     main()
