010b177674c188b9a2faa687d91c4a4172301086
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-10 17:20:55 -0600

    temporarily hold on week5 homework

--- week5/test_twttr.py
+++ week5/test_twttr.py
@@ -1,8 +1,10 @@
 
-import pytest
 from twttr import shorten
 
 
 def test_shorten_function():
     assert shorten("Twitter") == "Twttr"
-    assert shorten("What is your name?") == "Wht's yr nm?"
+    assert shorten("twitter") == "twttr"
+    assert shorten("What is your name?") == "Whts yr nm?"
+    assert shorten("TWITTER") == "TWTTR"
+
