3f4f15d09b559bab63b491dd03cf67fdc7b51b28
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-10 18:49:02 -0600

    Week 6 lesson

---
+++ names.py
@@ -0,0 +1,19 @@
+# names = []
+
+# for _ in range(3):
+#     names.append(input("What's your name?"))
+
+# for name in sorted(names):
+#     print(f" hello, {name}")
+
+# ----------------------------------------------------------------------------
+
+name = input("What's your name?")
+
+
+file = open("names.txt", "a")
+file.write(name)
+file.close()
+
+
+# ----------------------------------------------------------------------------
