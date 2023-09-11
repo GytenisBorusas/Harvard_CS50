80fb4e7d49a509e99e9fd65e5fdb7a7ff9034009
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-10 19:12:34 -0600

    final commit for the day

--- names.py
+++ names.py
@@ -8,12 +8,72 @@
 
 # ----------------------------------------------------------------------------
 
-name = input("What's your name?")
+# name = input("What's your name?")
 
 
-file = open("names.txt", "a")
-file.write(name)
-file.close()
-
+# file = open("names.txt", "a")
+# file.write(f"{name}\n")
+# file.close()
 
 # ----------------------------------------------------------------------------
+
+# name = input("What's your name?")
+
+
+# with open("names.txt", "a") as file:
+#     file.write(f"{name}\n")
+
+# ----------------------------------------------------------------------------
+
+##### read file content
+
+# with open("names.txt", "r") as file:
+#     lines = file.readlines()
+
+# for line in lines:
+#     print("hello,", line.rstrip())
+
+# ----------------------------------------------------------------------------
+
+### same as above but more condenced
+
+# with open("names.txt", "r") as file:
+#     for line in file:
+#         print("hello,", line.rstrip())
+
+# ----------------------------------------------------------------------------
+
+### read all the lines, sort them and then print
+### it
+
+# names = []
+
+# with open("names.txt") as file:
+#     for line in file:
+#         names.append(line.rstrip())
+
+# for name in sorted(names):
+#     print(f"hello, {name}")
+
+ # ----------------------------------------------------------------------------
+
+### read all the lines, sort them and then print
+### it. Sane as above, but more condensed.
+
+# with open("names.txt") as file:
+#     for line in sorted(file):
+#         print(f"hello,", line.rstrip()) 
+
+ # ----------------------------------------------------------------------------
+
+### read all the lines, sort them and then print
+### it. Sane as above, but more condensed.
+
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {name}")
