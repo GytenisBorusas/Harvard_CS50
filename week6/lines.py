21a6c233b174926011b072c358d32f36bd0bbc46
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-12 13:51:21 -0600

    testing and troubleshooting lines.py

--- week6/lines.py
+++ week6/lines.py
@@ -3,14 +3,27 @@ import sys
 
 
 def main():
-    total_lines = imported_file()
-    print(f"{total_lines}")
+    if len(sys.argv) < 2:
+        print("Too few comman-line arguments")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("Too many comman-line arguments")
+        sys.exit(1)
+    elif not sys.argv[1].endswith('.py'):
+        print("Not a Python file")
+        sys.exit(1)
+    
+    try:
+        total_lines = imported_file(sys.argv[1])
+        print(f"{total_lines}")
+    except FileNotFoundError:
+        print("file does not exist")
+        sys.exit(1)
 
 
 
-
-def imported_file():
-    with open("hello.py", "r") as file:
+def imported_file(filename):
+    with open(filename, "r") as file:
         lines = file.readlines()
         non_comment_lines  = [line for line in lines if not line.lstrip().startswith("#")]
         num_lines = len(non_comment_lines)
