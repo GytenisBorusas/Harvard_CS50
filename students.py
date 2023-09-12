8a2947fdada17e655a4f6871671173dbc709474e
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-12 09:19:23 -0600

    modified:   students.py

--- students.py
+++ students.py
@@ -34,6 +34,13 @@ with open("students.csv") as file:
         student = {"name": name, "house": house}
         students.append(student)
 
-for student in students:
+
+def get_name(student):
+    return student["name"]
+
+def get_house(student):
+    return student["house"]
+
+for student in sorted(students, key=get_house, reverse=True):
     print(f"{student['name']} is in {student['house']}")
 
