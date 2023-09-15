913930f6244251c66d983823c9d7a82fe5eef787
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-15 12:20:28 -0600

    firt commit

--- validate.py
+++ validate.py
@@ -1,14 +1,16 @@
 
 import re
-
+import csv
 
 
 email = input("What's your email? ").strip()
 
-if re.search(".*@.*", email):
+if re.search(".+@.+", email):
     print("valid")
 else:
     print("Invalid")
+
+
 
 
 
