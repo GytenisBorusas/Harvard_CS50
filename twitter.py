3a9d139c2430633ffdb3e8a77885d81b27379a76
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-16 20:08:07 -0600

    last commit tonight

--- twitter.py
+++ twitter.py
@@ -4,5 +4,14 @@ import re
 url = input("URL: ").strip()
 
 
-username = re.sub(r"https//twitter.com/", "", url)
-print(f"Username: {username}")
+# username = re.sub(r"^(https?//)?(www\.)?twitter\.com/", "", url)
+# print(f"Username: {username}")
+
+
+# matches = re.search(r"^https?://(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
+# if matches:
+#     print(f"Username:", matches.group(2))
+
+#same as above
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)$", url, re.IGNORECASE)
+print(f"Username:", matches.group(1))
