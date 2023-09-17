c30b657595c7668f20205983feaf905479cd87b0
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-16 18:21:42 -0600

    classword 7

--- format.py
+++ format.py
@@ -1,3 +1,30 @@
 
+# import re
 
+# name = input("What's your name? ").strip()
+# if "," in name:
+#     last, first = name.split(", ?")
+#     name = f"{first} {last}"
 
+# print(f"hello, {name}")
+
+# ------------------------------------
+
+# import re
+
+# name = input("What's your name? ").strip()
+# matches = re.search(r"^(.+), (.+)$", name)
+# if matches:
+#     last, first = matches.groups()
+#     name = f"{first} {last}"
+# print(f"hello, {name}")
+
+# ------------------------------------
+
+import re
+
+name = input("What's your name? ").strip()
+matches = re.search(r"^(.+), *(.+)$", name)
+if matches:
+    name = matches.group(2) + " " + matches.groups(1)
+print(f"hello, {name}")
