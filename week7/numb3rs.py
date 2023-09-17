5bad6cb942604607079b09c9dadc2b484fdc40f5
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-17 13:59:21 -0600

    first week7 hw commit

---
+++ week7/numb3rs.py
@@ -0,0 +1,22 @@
+
+
+import re
+import sys
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+
+# Expects an IPv4 address as input as a str and 
+# then returns True or False, respectively, if that input is a valid IPv4 address or not
+def validate(ip):
+    user_input = str(input("IPv4 Address: "))
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
