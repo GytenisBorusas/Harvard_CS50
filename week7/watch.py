e14557ec354945b083731f31ac5158572e741805
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-19 18:02:11 -0600

    start of troubleshooting

--- week7/watch.py
+++ week7/watch.py
@@ -5,15 +5,19 @@ import sys
 
 
 def main():
-    print(parse(input("HTML: ")))
+    s = input("HTML: ")
+    returned_s = parse(s)
+    if returned_s:
+        print(returned_s)
+    else:
+        print(f"No valid URL")
 
 
 def parse(s):
-    
-    if matches := re.search(r"^https?://(?:www\.)?youtube\.com/embed/([a-z0-9]+)$", url, re.IGNORECASE):
-        print(f"Match URL:", matches.group(1))
-
-    return 
+    if matches := re.search(r'src="https?://(?:www\.)?youtube\.com/embed/([a-zA-Z0-9_-]+)"', s, re.IGNORECASE):
+        video_id = matches.group(1)
+        return f"https://youtu.be/{video_id}"
+    return None
 
 
 if __name__ == "__main__":
