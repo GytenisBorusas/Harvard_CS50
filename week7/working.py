1ebb69a101b124f8384e0f327ea23cbe5b851240
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-19 19:40:21 -0600

    needs to implement invalid times

--- week7/working.py
+++ week7/working.py
@@ -3,7 +3,6 @@
 import re
 import sys
 
-
 def main():
     s = input("Hours: ")
     time_from, time_to = convert(s)
@@ -11,13 +10,13 @@ def main():
 
 
 def convert(s):
-    match = re.search(r'(\d{1,2}(:\d{2})?\s*[APM]{2})\s*to\s*(\d{1,2}(:\d{2})?\s*[APM]{2})', s, re.IGNORECASE):
-    first_hour = matches.group(1)
-    first_minute = matches.group(2)
-    first_am_or_pm = matches.group(3)
-    second_hour = matches.group(5)
-    second_minute = matches.group(6)
-    second_am_or_pm = matches.group(7)
+    match = re.search(r'(\d{1,2})(:(\d{2}))?\s*([APM]{2})\s*to\s*(\d{1,2})(:(\d{2}))?\s*([APM]{2})', s, re.IGNORECASE)
+    first_hour = match.group(1)
+    first_minute = match.group(3) if match.group(3) else '00'
+    first_am_or_pm = match.group(4)
+    second_hour = match.group(5)
+    second_minute = match.group(7) if match.group(7) else '00'
+    second_am_or_pm = match.group(8)
 
     if first_am_or_pm == "PM":
         from_hours = int(first_hour) + 12
@@ -31,11 +30,10 @@ def convert(s):
 
 
 
-        time_from = str(f"{from_hours}:{first_minute}")
-        time_to = str(f"{to_hours}:{second_minute}")
+    time_from = str(f"{from_hours}:{first_minute}")
+    time_to = str(f"{to_hours}:{second_minute}")
 
-        return time_from, time_to
-    return None
+    return time_from, time_to
 
 
 if __name__ == "__main__":
