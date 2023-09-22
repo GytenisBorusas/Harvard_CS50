6fb013faf689c123107e505e722e64eafe8085fa
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-21 19:51:19 -0600

    mid class work week 8

--- student.py
+++ student.py
@@ -126,11 +126,14 @@ class Student:
         if house not in ["Griff", "Huff", "Raven", "Slyth"]:
             raise ValueError("Invalid house")
         self.name = name 
-        self.name = house
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 def get_student():
     name = input("Name: ")
