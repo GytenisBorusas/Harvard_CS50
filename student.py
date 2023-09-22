70f0f38895f5a7d76ed8ce9a909b921e8467282e
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-21 20:48:59 -0600

    getter and setter

--- student.py
+++ student.py
@@ -120,39 +120,37 @@
 
 # learning class'es
 class Student:
-    def __init__(self, name, house, patronus):
+    def __init__(self, name, house):
         if not name:
             raise ValueError("Missing name")
-        if house not in ["Griff", "Huff", "Raven", "Slyth"]:
-            raise ValueError("Invalid house")
         self.name = name 
         self.house = house
-        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
     
-    def charm(self):
-        match self.patronus:
-            case "Stag":
-                return "🐎"
-            case "Otter":
-                return "🦦"
-            case "Jack":
-                return "🐶"
-            case _:
-                return "👉"
+    # Getter
+    @property 
+    def house(self):
+        return self._house
+    
+    # Setter
+    @house.setter 
+    def house(self, house):
+        if house not in ["Griff", "Huff", "Raven", "Slyth"]:
+            raise ValueError("Invalid house")
+        self._house = house
+    
 
 def main():
     student = get_student()
-    print("Expecto Patronum!")
-    print(student.charm())
+    print(student)
+
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    patronus = input("Patronus: ")
-    return Student(name, house, patronus)
+    return Student(name, house)
 
 
 if __name__ == "__main__":
