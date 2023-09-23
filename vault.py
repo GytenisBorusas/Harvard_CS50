60026cf21bb1461a8921d44bac481285e0c53a35
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-22 18:48:10 -0600

    beggining of seasons.py

--- vault.py
+++ vault.py
@@ -10,11 +10,19 @@ class Vault:
     def __str__(self):
         return f"{self.galleons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
 
-potter = Vault(100, 50, 20)
+    def __add__(self, other):
+        galleons = self.galleons + other.galleons
+        sickles = self.sickles + other.sickles
+        knuts = self.knuts + other.knuts
+        return Vault(galleons, sickles, knuts)
+
+potter = Vault(100, 50, 25)
 print(potter)
 
-weasley = Vault(20, 50, 100)
+weasley = Vault(25, 50, 100)
 print(weasley)
 
 
+total = potter + weasley
+print(total)
 
