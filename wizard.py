661018bcf10b893fdc69da8deaf39f0156ef6972
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-22 18:07:53 -0600

    almost done with classwork

--- wizard.py
+++ wizard.py
@@ -9,15 +9,24 @@ class Wizard:
 
 class Student(Wizard):
     def __init__(self, name, house):
+        super().__init__(name)
         self.house = house
 
     ...
 
 class Professor(Wizard):
     def __init__(self, name, subject):
+        super().__init__(name)
         self.subject = subject
 
     ...
+
+
+wizard = Wizard("Albus")
+student = Student("Harry", "Griff")
+professor = Professor("Severus", "Defence against the dark arts")
+
+
 
 
 
