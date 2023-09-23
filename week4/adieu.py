4d5611bfa996e0ca7ae2f3941a1d3074b6870b24
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-22 20:38:14 -0600

    fixed adieu.py error and submitted it

--- week4/adieu.py
+++ week4/adieu.py
@@ -4,10 +4,9 @@ p = inflect.engine()
 
 def main():
     all_names = get_all_names()
-
     message_to_print = message_function(all_names, p)
-
     print(message_to_print)
+
 
 def get_all_names():
     names_list = []
@@ -18,12 +17,12 @@ def get_all_names():
                 names_list.append(name)
     except EOFError:
         pass
-
     return names_list
 
-def message_function(all_names, p):
-    joined_names = p.join(all_names, final_sep=" and ")
 
+def message_function(all_names, p):
+    # joined_names = p.join(all_names, final_sep=" and ")
+    joined_names = p.join(all_names)
     return f"Adieu, adieu, to {joined_names}".strip()
 
 if __name__ == "__main__":
