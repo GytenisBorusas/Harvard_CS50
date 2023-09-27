b37bcc29b14e68860cb8fa6aae9a190bc8117f35
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-09-26 18:53:44 -0600

    cant figure it out

--- week3/fuel.py
+++ week3/fuel.py
@@ -2,7 +2,7 @@
 
 # def main():
 #     fuel_amount  = fraction_input("Fraction? ")
-    
+
 #     if fuel_amount <= 1:
 #         print("E")
 #     elif fuel_amount >= 99:
@@ -17,7 +17,7 @@
 #         try:
 #             # get input from the user
 #             fuel_fraction = input(prompt)
-            
+
 #             # divides input 'fuel_fraction' into 2 different variables - 'x' and 'y'
 #             x, y = fuel_fraction.split('/')
 
@@ -39,7 +39,7 @@
 #             print("Cant divide by 0")
 #         else:
 #             return fuel_amount
-        
+
 
 # if __name__ == "__main__":
 #     main()
@@ -61,7 +61,7 @@ def convert(fraction):
         try:
             # get input from the user
             # fuel_fraction = input(prompt)
-            
+
             # divides input 'fraction' into 2 different variables - 'x' and 'y'
             x, y = fraction.split('/')
 
@@ -83,7 +83,110 @@ def convert(fraction):
         except ZeroDivisionError:
             print("Can't divide by 0")
             continue
-    
+
+        return str(fuel_amount)
+
+
+def gauge(percentage):
+
+    percentage = int(percentage)
+
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return f"{percentage}%"
+
+
+
+if __name__ == "__main__":
+    main()# week3 submition
+
+# def main():
+#     fuel_amount  = fraction_input("Fraction? ")
+
+#     if fuel_amount <= 1:
+#         print("E")
+#     elif fuel_amount >= 99:
+#         print("F")
+#     else:
+#         print(f"{fuel_amount}%")
+
+
+# def fraction_input(prompt):
+
+#     while True:
+#         try:
+#             # get input from the user
+#             fuel_fraction = input(prompt)
+
+#             # divides input 'fuel_fraction' into 2 different variables - 'x' and 'y'
+#             x, y = fuel_fraction.split('/')
+
+#             # Converts 'x' and 'y' to integers
+#             x = int(x)
+#             y = int(y)
+
+#             # Converts fuel amount into integer number and rounds it up
+#             fuel_amount = round(x / y * 100)
+
+#             # Check if fuel_amount is greater than 100
+#             if fuel_amount > 100:
+#                 print("Above full tank")
+#                 continue
+
+#         except ValueError:
+#             print("This isnt a fraction")
+#         except ZeroDivisionError:
+#             print("Cant divide by 0")
+#         else:
+#             return fuel_amount
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+# ----------------------------------------
+# week 5 submition
+
+import sys
+
+def main():
+    user_input_in_fraction  = input("Fraction? ")
+    converted_fuel_amount = convert(user_input_in_fraction)
+    fuel_indicator = gauge(converted_fuel_amount)
+    print(fuel_indicator)
+
+def convert(fraction):
+    while True:
+        try:
+            # get input from the user
+            # fuel_fraction = input(prompt)
+
+            # divides input 'fraction' into 2 different variables - 'x' and 'y'
+            x, y = fraction.split('/')
+
+            # Converts 'x' and 'y' to integers
+            x = int(x)
+            y = int(y)
+
+            # Converts fuel amount into integer number and rounds it up
+            fuel_amount = round(x / y * 100)
+
+            # Check if fuel_amount is greater than 100
+            if fuel_amount > 100:
+                print("Above full tank")
+                continue
+
+        except ValueError:
+            print("This isn't a fraction")
+            continue
+        except ZeroDivisionError:
+            print("Can't divide by 0")
+            continue
+
         return str(fuel_amount)
 
 
--- week5/test_fuel.py
+++ week5/test_fuel.py
@@ -10,18 +10,12 @@ def test_1():
 
 def test_2():
     assert convert("1/10") == "10"
-
-# def test_3():
-#     assert convert("TWITTER") == "TWTTR"
-
+    
 def test_4():
-    assert gauge(75) == "75%"
+    assert gauge("75") == "75%"
 
 def test_5():
-    assert gauge(100) == "F"
+    assert gauge("100") == "F"
 
 def test_6():
-    assert gauge(1) == "E"
-
-# def test_7():
-#     assert gauge("twit..ter") == "twt..tr"
+    assert gauge("1") == "E"
