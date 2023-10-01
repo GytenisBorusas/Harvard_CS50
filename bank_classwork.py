8a953fb718be467ce0d4600c9681f2e4b8f664e7
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-10-01 14:51:20 -0600

    classwork bank_classwork.py

--- bank_classwork.py
+++ bank_classwork.py
@@ -14,3 +14,14 @@ class Account:
         self._balance -= n
 
 def main():
+    account = Account()
+    print("Balance: ", account.balance)
+    account.deposit(100)
+    account.withdraw(50)
+    print("Balance: ", account.balance)
+
+
+if __name__ == "__main__":
+    main()
+
+
