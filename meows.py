ef51c1946fc41479490499769362152d243fdcde
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-10-02 17:56:52 -0600

    first commit

--- meows.py
+++ meows.py
@@ -1,15 +1,26 @@
 
 
-class Cat:
-    MEOWS = 3
+# class Cat:
+#     MEOWS = 3
 
-    def meow(self):
-        for _ in range(Cat.MEOWS):
-            print("meow")
+#     def meow(self):
+#         for _ in range(Cat.MEOWS):
+#             print("meow")
 
-cat = Cat()
-cat.meow()
+# cat = Cat()
+# cat.meow()
 
+
+# --------------------------------------------------------
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+number = input("Number: ")
+meow(number)
 
 
 
