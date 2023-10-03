d7ca9928cbcee0277b2096a1c1c3ef19496e6a0c
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-10-02 20:05:21 -0600

    more meow variants

--- meows.py
+++ meows.py
@@ -14,13 +14,13 @@
 # --------------------------------------------------------
 
 
-def meow(n: int):
-    for _ in range(n):
-        print("meow")
+def meow(n: int) -> str:
+    return "meow\n" * n
 
 
-number: int = input("Number: ")
-meow(number)
+number: int = int(input("Number: "))
+meows: str = meow(number)
+print(meows, end="")
 
 
 
