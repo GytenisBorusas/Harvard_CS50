daa8e4edb49ed26d6a7a96fd792d74d5a9018522
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-10-04 11:53:15 -0600

    first commit

--- unpack.py
+++ unpack.py
@@ -1,12 +1,14 @@
 
 
-
-first, last = input("What's your name? ").split(" ")
-
-print(f"hello, {first}")
+def total(galleons, sickles, knuts):
+    return (galleons * 17 + sickles) * 29 + knuts
 
 
 
+
+coins = {"galleons":100, "sickles":50, "knuts":25}
+
+print(total(galleons=100, sickles=50, knuts=25), "Knuts")
 
 
 
